278f9addf66a65d808049aba7d5dc9359831da25
Prince Singh <prince.dev205@gmail.com>
2023-11-08 17:54:40 +0530

    implelment base tailwind config and style guide

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main>Dev over flow</main>;
+  return (
+    <main>
+      <h1 className="h1-bold">Dev over flow</h1>
+      <h1 className="h2-bold">Dev over flow</h1>
+      <h1 className="h3-bold">Dev over flow</h1>
+    </main>
+  );
 }
