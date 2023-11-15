8b99e7040e45393c05f7ece9722fea614bd4a2f4
Prince Singh <prince.dev205@gmail.com>
2023-11-15 11:04:10 +0530

    add local searchbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -14,7 +15,14 @@ export default function Home() {
       </div>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        LocalSearchbar Filters
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imageScr="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />{" "}
+        Filters
       </div>
     </>
   );
