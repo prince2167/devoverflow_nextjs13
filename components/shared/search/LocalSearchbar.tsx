9603c5b2e936fac88e011bb21a24e79008009a92
Prince Singh <prince.dev205@gmail.com>
2023-11-15 12:31:56 +0530

    create home page filters

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -37,7 +37,7 @@ const LocalSearchbar = ({
         placeholder={placeholder}
         value=""
         onChange={() => {}}
-        className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
       />
 
       {iconPosition === "right" && (
