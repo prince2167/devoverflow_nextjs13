1d72cc89d674159efd613d28a2e863ae1ef147be
Prince Singh <prince.dev205@gmail.com>
2023-11-16 22:15:29 +0530

    create ask question form & add markdown

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Question from "@/components/forms/Question";
 
-const AskQuestions = () => {
-  return <div>AskQuestions</div>;
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default AskQuestions;
+export default Page;
