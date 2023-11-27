535911b427fef904822927e5720232f142757d2d
Prince Singh <prince.dev205@gmail.com>
2023-11-27 11:44:47 +0530

    refactor middleware

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
 
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     // TOdo
-    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+    const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
     if (!WEBHOOK_SECRET) {
         throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
@@ -37,6 +37,8 @@ export async function POST(req: Request) {
 
     let evt: WebhookEvent
 
+
+
     // Verify the payload with the headers
     try {
         evt = wh.verify(body, {
@@ -53,7 +55,7 @@ export async function POST(req: Request) {
 
     // Get the ID and type
     const eventType = evt.type;
-
+    console.log({ eventType })
     if (eventType === "user.created") {
         const { id, email_addresses, image_url, username, first_name, last_name } = evt.data
         //  create new user in database
